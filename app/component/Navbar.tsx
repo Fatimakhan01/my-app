8e3082689e7ac7935e620b192e8682279b820fb9
Fatimakhan01 <fatimayousaf597@gmail.com>
2025-10-02 19:10:14 +0500

    Initial commit

---
+++ app/component/Navbar.tsx
@@ -0,0 +1,94 @@
+import React from 'react';
+import Image from 'next/image';
+import { ArrowRight,RefreshCcw, Heart,ShoppingCart,User } from 'lucide-react';
+
+const Navbar = () => {
+  return (
+  <header className='w-full border-b border-[#DEDFE2] bg-white'>
+    <div className='max-w-[75rem] mx-auto h-[6.3rem]  flex items-center justify-between font-Lato gap-2'>
+        <div className='flex items-center gap-6 w-full'>
+     <img
+     src='/Nest.png'
+     alt='logo'
+     />
+      
+      <form className='flex border border-[#BCE3C9] text-sm rounded w-full max-w-md '>
+  <input
+    type='text'
+    placeholder='Search for products..'
+    className='flex-1 h-[2.6rem] px-3 outline-none text-[#999999]'
+  />
+  <button className='bg-[#3BB77E] text-[#FFFFFF] px-4 m-1 rounded  '>
+    Search
+  </button>
+</form>
+
+    <button className='flex items-center gap-1 border px-4 py-2 rounded text-sm
+     bg-[#FFFFFF] text-[#3BB77E] shadow-md hover:shadow-lg transition whitespace-nowrap'>
+  Became Vendor
+  <ArrowRight 
+  size={20}
+  className='text-[#3BB77E]' />
+</button>
+</div>
+
+ <nav className='flex items-center gap-4 font-Lato text-sm text-[#7E7E7E]'>
+   <button className='relative flex items-center gap-1'>
+     <div className='relative'>
+      <RefreshCcw 
+      size={22} 
+      className='text-[#253D4E]' />
+      <span className='absolute -top-2 -right-2 bg-[#3BB77E] text-white
+       text-[10px] w-4 h-4 flex items-center justify-center rounded-full'>
+       0
+      </span>
+     </div>
+      <span>Compare</span>
+    </button>
+
+   <button className=' relative flex items-center gap-1'>
+    <div className='relative'>
+    <Heart
+     size={22}
+     className='text-[#253D4E]' />
+     <span className='absolute -top-2 -right-2 bg-[#3BB77E] text-white
+     text-[10px] w-4 h-4 flex items-center justify-center rounded-full'>
+      0
+     </span>
+     </div>
+    <span>Wishlist</span>
+   </button>
+
+   <button className=' relative flex items-center gap-1'>
+    <div className='relative'>
+    <ShoppingCart 
+    size={22} 
+    className='text-[#253D4E]' />
+    <span className='absolute -top-2 -right-2 bg-[#3BB77E] text-white
+     text-[10px] w-4 h-4 flex items-center justify-center rounded-full'>
+      0
+     </span>
+    </div>
+    <span>Cart</span>
+   </button>
+
+   <button className=' relative flex items-center gap-1'>
+    <div className='relative'>
+    <User
+     size={22}
+     className='text-[#253D4E]' />
+     <span className='absolute -top-2 -right-2 bg-[#3BB77E] text-white
+     text-[10px] w-4 h-4 flex items-center justify-center rounded-full'>
+      0
+     </span>
+     </div>
+    <span>Account</span>
+   </button>
+
+ </nav>
+   </div>
+    </header>
+  )
+}
+
+export default Navbar
---
+++ app/component/NavbarTop.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { ChevronDown } from 'lucide-react';
+
+
+const NavbarTop = () => {
+  return (
+    <header className='w-full border-b border-[#DEDFE2] bg-white'>
+  <div className='max-w-[75rem] mx-auto px-4 h-[2.5625rem] flex items-center 
+  justify-between text-sm font-Lato'>
+
+      <nav>
+          <ul className='flex items-center space-x-2 text-[#7E7E7E] '>
+            <li>About Us</li>
+            <span className='w-px h-2.5 bg-[#DEDFE2]' />
+            <li>My Account</li>
+            <span className='w-px h-2.5 bg-[#DEDFE2]' />
+            <li>Wishlist</li>
+            <span className='w-px h-2.5 bg-[#DEDFE2]' />
+            <li>Order Tracking</li>
+          </ul>
+       </nav>
+
+      <p className='text-[#3BB77E] text-md font-bold mt-2 md:mt-0'>
+        100% Secure delivery without contacting the courier
+      </p>
+
+      <div className=' flex items-center space-x-2 text-[#7E7E7E]'>
+        <span> Need help? Call Us:</span>
+        <span className='text-[#3BB77E] font-semibold'>+1800900122</span>
+        <span className='w-px h-2.5 bg-[#DEDFE2]'></span>
+         <button className='flex items-center gap-1'>
+            English <ChevronDown size={14} />
+         </button>
+        <span className='w-px h-2.5 bg-[#DEDFE2]' />
+         <button className='flex items-center gap-1'>
+            INR <ChevronDown size={14} />
+         </button>
+      </div>
+    </div>
+    </header>
+  )
+
+  }
+export default NavbarTop
